8f3eef762a51884f7fb8d9ee70af4d172b575dc4
skennel <skennel@douzone.com>
2022-01-03 14:05:21 +0900

    dotenv config 옵션 설명 추가

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv'
+import path from path;
+
 dotenv.config()
 
-console.log(process.env)
+// dotenv.config({
+//     path: 환경설정파일 경로, // Default: path.resolve(process.cwd(), '.env')
+//     encoding: 'latin1', // Default: utf8
+//     debug:  // Default: false
+// })
+
 if (process.env.NODE_ENV === 'product') {
     console.log('product mode start')
 } else if (process.env.NODE_ENV === 'develop') {
